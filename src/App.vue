1b85a0f79ea1fce14461b7f0a1e7886fcfb12183
lizhihua <lizhihua_ok@126.com>
2020-06-10 11:13:59 +0800

    =update

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+    <HelloWorld
+      v-if="visible"
+      msg="Welcome to Your Vue.js App"
+    />
+
+    <button @click="toggle">
+      toggle
+    </button>
+  </div>
+</template>
+
+<script>
+import { reactive, toRefs } from '@vue/composition-api';
+import HelloWorld from './components/HelloWorld.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+  },
+  setup() {
+    const state = reactive({
+      visible: true,
+    });
+
+    const toggle = () => {
+      state.visible = !state.visible;
+    };
+
+    return {
+      ...toRefs(state),
+      toggle,
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="hello">
+    {{ msg }}
+  </div>
+</template>
+
+<script>
+import Vue from 'vue';
+import { onMounted } from '@vue/composition-api';
+
+// use Vue.extend
+export default Vue.extend({
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+  },
+  mixins: [
+    {
+      // normal
+      mounted() {
+        console.log('mixin1 mounted');
+      },
+    },
+
+    {
+      // called twice when re-create
+      setup() {
+        onMounted(() => {
+          console.log('mixin2 onMounted');
+        });
+      },
+    },
+  ],
+
+  setup() {
+    onMounted(() => {
+      console.log('onMounted');
+    });
+  },
+});
+</script>
